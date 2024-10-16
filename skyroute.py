18212fd4551b6cfab225eef53696e3253eda6492
HAMED-AYOUB <100809816+HAMED-AYOUB@users.noreply.github.com>
2024-10-16 01:06:43 +0100

    Update skyroute.py

--- skyroute.py
+++ skyroute.py
@@ -1,9 +1,9 @@
-dirfrom graph_search import bfs, dfs
+from graph_search import bfs, dfs
 from vc_metro import vc_metro
 from vc_landmarks import vc_landmarks
 from landmark_choices import landmark_choices
 
-# Build your program below:
+
 landmark_string = ""
 for letter, landmark in landmark_choices.items():
     landmark_string += "{0} - {1}\n".format(letter, landmark)
